36d3326888e0fe1b2af1ec2f5f1c3bde60dad9c3
Richard Challis <rjchallis@gmail.com>
2017-06-30 14:27:16 +0000

    restore internal urls

--- js/functions.js
+++ js/functions.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
     $('html, body').stop().animate({
       'scrollTop': $target.offset().top
     }, 900, 'swing', function () {
-      //window.location.hash = target;
+      window.location.hash = target;
     });
 	});
 
